2b2f6f98a4ecc34b4cfb4de704029d983acf3233
GuptaAnupriya <anupriya263@gmail.com>
2020-05-24 12:40:17 +0530

    Pushing the code2nd time

--- Priti Project1/src/pkg16may/Program1.java
+++ Priti Project1/src/pkg16may/Program1.java
@@ -3,11 +3,14 @@ package pkg16may;
 public class Program1 {
 	
 	public static void main(String[] args) {
+		
 		for(int i=1;i<=5;i++)
 		{
 			for(int j=1;j<=i;j++)
 			{
 				System.out.print("*");
+				System.out.println("Changes done in a program");
+				
 			}
 			System.out.println();
 		}
